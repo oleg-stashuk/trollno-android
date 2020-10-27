a9ad8245e0cd1dbfeae35ed0a4b084fcf5558160
Nina <n.kruchinkina22022@gmail.com>
2020-10-27 18:03:50 +0200

    Add EditUserProfileActivity.

---
+++ app/src/main/java/com/apps/trollino/ui/main_group/EditUserProfileActivity.java
@@ -0,0 +1,24 @@
+package com.apps.trollino.ui.main_group;
+
+import android.view.View;
+
+import com.apps.trollino.R;
+import com.apps.trollino.ui.base.BaseActivity;
+
+public class EditUserProfileActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_edit_user_profile;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
