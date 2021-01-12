40e40ddad5933ee2f00e2db335f7f75ba09ddf0b
Nina <n.kruchinkina22022@gmail.com>
2021-01-12 15:16:28 +0200

    In TapeActivity add show recycler in this activity for Category from API. Change OtherCategoryPostFragment.

--- app/src/main/java/com/apps/trollino/ui/fragment/OtherCategoryPostFragment.java
+++ app/src/main/java/com/apps/trollino/ui/fragment/OtherCategoryPostFragment.java
@@ -2,6 +2,7 @@ package com.apps.trollino.ui.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,14 +11,20 @@ import android.widget.ProgressBar;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.apps.trollino.R;
+import com.apps.trollino.adapters.PostListAdapter;
+import com.apps.trollino.data.model.PostsModel;
+import com.apps.trollino.ui.base.BaseActivity;
+import com.apps.trollino.utils.RecyclerScrollListener;
 import com.apps.trollino.utils.data.PostListByCategoryFromApi;
 import com.apps.trollino.utils.data.PrefUtils;
+import com.apps.trollino.utils.networking.main_group.GetPostsByCategory;
 
 import static android.content.Context.MODE_PRIVATE;
-import static com.apps.trollino.utils.recycler.MakePostsByCategoryGridRecyclerViewForTapeActivity.makePostsByCategoryGridRecyclerViewForTapeActivity;
+import static com.apps.trollino.utils.OpenPostActivityHelper.openPostActivity;
 
 public class OtherCategoryPostFragment extends Fragment {
 
@@ -44,10 +51,39 @@ public class OtherCategoryPostFragment extends Fragment {
         recyclerView = view.findViewById(R.id.other_category_recycler_tape);
         progressBar = view.findViewById(R.id.other_category_progress_bar_tape);
 
-//        prefUtils.saveSelectedCategoryId(tab.getTag().toString());
-//        PostListByCategoryFromApi.getInstance().removeAllDataFromList(prefUtils);
-//        makePostsByCategoryGridRecyclerViewForTapeActivity(getContext(), recyclerView, progressBar, prefUtils);
-
+//        makeGridRecycler();
+        recyclerView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
         return view;
     }
+
+    public void makeGridRecycler() {
+        PostListAdapter adapter = new PostListAdapter((BaseActivity) getContext(), prefUtils, PostListByCategoryFromApi.getInstance().getPostListByCategory(), newPostsItemListener);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        if(PostListByCategoryFromApi.getInstance().getPostListByCategory().isEmpty()) {
+            progressBar.setVisibility(View.VISIBLE);
+            Handler handler = new Handler();
+            handler.postDelayed(() -> new Thread(() -> {
+                    GetPostsByCategory.getPostsByCategory(getContext(), prefUtils, adapter, progressBar);
+            }).start(), 1000);
+        }
+
+        recyclerView.addOnScrollListener(new RecyclerScrollListener() {
+            @Override
+            public void onScrolledToEnd() {
+                progressBar.setVisibility(View.VISIBLE);
+                Handler handler = new Handler();
+                handler.postDelayed(() -> new Thread(() -> {
+                        GetPostsByCategory.getPostsByCategory(getContext(), prefUtils, adapter, progressBar);
+                }).start(), 1000);
+            }
+        });
+    }
+
+    // Обработка нажатия на элемент списка
+    private final PostListAdapter.OnItemClick<PostsModel.PostDetails> newPostsItemListener = (item, position) -> {
+        openPostActivity(getContext(), item, prefUtils, true);
+    };
 }
--- app/src/main/java/com/apps/trollino/ui/main_group/TapeActivity.java
+++ app/src/main/java/com/apps/trollino/ui/main_group/TapeActivity.java
@@ -3,18 +3,21 @@ package com.apps.trollino.ui.main_group;
 import android.content.Intent;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import com.apps.trollino.R;
 import com.apps.trollino.data.model.CategoryModel;
+import com.apps.trollino.ui.base.BaseActivity;
 import com.apps.trollino.ui.fragment.DiscussPostFragment;
 import com.apps.trollino.ui.fragment.FreshPostFragment;
 import com.apps.trollino.ui.fragment.OtherCategoryPostFragment;
-import com.apps.trollino.ui.base.BaseActivity;
 import com.apps.trollino.utils.ViewPagerAdapter;
 import com.apps.trollino.utils.data.DataListFromApi;
 import com.apps.trollino.utils.data.PostListByCategoryFromApi;
+import com.apps.trollino.utils.recycler.MakePostsByCategoryGridRecyclerViewForTapeActivity;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.List;
@@ -22,6 +25,11 @@ import java.util.List;
 public class TapeActivity extends BaseActivity implements View.OnClickListener{
     private TabLayout tabs;
     private ViewPager viewPager;
+
+    private int pagerPosition;
+    private List<CategoryModel> categoryList;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
 
     private boolean doubleBackToExitPressedOnce = false;  // для обработки нажатия onBackPressed
 
@@ -34,12 +42,16 @@ public class TapeActivity extends BaseActivity implements View.OnClickListener{
     protected void initView() {
         tabs = findViewById(R.id.tab_layout_tape);
         viewPager = findViewById(R.id.view_pager_tape);
+        recyclerView = findViewById(R.id.news_recycler_tape);
+        progressBar = findViewById(R.id.progress_bar_tape);
         findViewById(R.id.search_button_tape).setOnClickListener(this);
         findViewById(R.id.activity_button_tape).setOnClickListener(this);
         findViewById(R.id.favorites_button_tape).setOnClickListener(this);
         findViewById(R.id.profile_button_tape).setOnClickListener(this);
 
+        categoryList = prefUtils.getCategoryList();
         prefUtils.saveCurrentActivity("");
+        pagerPosition = viewPager.getCurrentItem();
         getTabs();
     }
 
@@ -49,127 +61,48 @@ public class TapeActivity extends BaseActivity implements View.OnClickListener{
             pagerAdapter.addFragment(FreshPostFragment.getInstance(), getResources().getString(R.string.fresh_post));
             pagerAdapter.addFragment(DiscussPostFragment.getInstance(), getResources().getString(R.string.discuss_post));
 
-            List<CategoryModel> categoryList = prefUtils.getCategoryList();
             for (CategoryModel category : categoryList) {
                 pagerAdapter.addFragment(OtherCategoryPostFragment.getInstance(), category.getNameCategory());
             }
 
             viewPager.setAdapter(pagerAdapter);
             tabs.setupWithViewPager(viewPager);
+
+            makePageChangeListener();
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        removeAllDataFromPostList();
-        if (doubleBackToExitPressedOnce) {
-            super.onBackPressed();
-            return;
-        }
-
-        this.doubleBackToExitPressedOnce = true;
-        showToast(getString(R.string.press_twice_to_exit));
-        new Handler().postDelayed(() -> doubleBackToExitPressedOnce=false, 2000);
-    }
-
-    private void removeAllDataFromPostList() {
-        DataListFromApi.getInstance().removeAllDataFromList(prefUtils);
-        PostListByCategoryFromApi.getInstance().removeAllDataFromList(prefUtils);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch(view.getId()) {
-            case R.id.search_button_tape: // "Перейти на экран Поиска"
-                removeAllDataFromPostList();
-                startActivity(new Intent(this, SearchActivity.class));
-                finish();
-                break;
-            case R.id.activity_button_tape: // "Перейти на экран Активность"
-                removeAllDataFromPostList();
-                startActivity(new Intent(this, ActivityInPostActivity.class));
-                finish();
-                break;
-            case R.id.favorites_button_tape: // "Перейти на экран Избранное"
-                removeAllDataFromPostList();
-                startActivity(new Intent(this, FavoriteActivity.class));
-                finish();
-                break;
-            case R.id.profile_button_tape: // "Перейти на экран Профиль"
-                removeAllDataFromPostList();
-                startActivity(new Intent(this, ProfileActivity.class));
-                finish();
-                break;
-        }
-    }
-}
-
-
-/*
-public class TapeActivity extends BaseActivity implements View.OnClickListener{
-    private RecyclerView newsRecyclerView;
-    private TabLayout tabs;
-    private ProgressBar progressBar;
-    private ViewPager viewPager;
-
-    private int selectedTabs;
-    private boolean doubleBackToExitPressedOnce = false;  // для обработки нажатия onBackPressed
-
-    @Override
-    protected int getLayoutID() {
-        return R.layout.activity_tape;
-    }
-
-    @Override
-    protected void initView() {
-        tabs = findViewById(R.id.tab_layout_tape);
-        viewPager = findViewById(R.id.view_pager_tape);
-        newsRecyclerView = findViewById(R.id.news_recycler_tape);
-        progressBar = findViewById(R.id.progress_bar_tape);
-        findViewById(R.id.search_button_tape).setOnClickListener(this);
-        findViewById(R.id.activity_button_tape).setOnClickListener(this);
-        findViewById(R.id.favorites_button_tape).setOnClickListener(this);
-        findViewById(R.id.profile_button_tape).setOnClickListener(this);
-
-        createTabLayout();
-
-        prefUtils.saveCurrentActivity("");
-        makeTabSelectedListener();
-        MakeGridRecyclerViewForTapeActivity.makeNewPostsRecyclerView(this, newsRecyclerView, progressBar, prefUtils);
-    }
-
-    // Add category list from Api to TabLayout
-    private void createTabLayout() {
-        List<CategoryModel> categoryList = prefUtils.getCategoryList();
-        for (CategoryModel category : categoryList) {
-            tabs.addTab(tabs.newTab().setText(category.getNameCategory()).setTag(category.getIdCategory()));
-        }
-    }
-
-    // Обработка нажатия на элементы горизонтального ScrollBar
-    private void makeTabSelectedListener() {
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+    private void makePageChangeListener() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                selectedTabs = tabs.getSelectedTabPosition();
-                progressBar.setVisibility(View.GONE);
-                if(tabs.getSelectedTabPosition() == 0) {
-                    MakeGridRecyclerViewForTapeActivity.makeNewPostsRecyclerView(TapeActivity.this, newsRecyclerView, progressBar, prefUtils);
-                } else if(tabs.getSelectedTabPosition() == 1) {
-                    MakeLinerRecyclerViewForTapeActivity.makeLinerRecyclerViewForTapeActivity(TapeActivity.this, newsRecyclerView, progressBar, prefUtils);
-                } else {
-                    prefUtils.saveSelectedCategoryId(tab.getTag().toString());
-                    PostListByCategoryFromApi.getInstance().removeAllDataFromList(prefUtils);
-                    makePostsByCategoryGridRecyclerViewForTapeActivity(TapeActivity.this, newsRecyclerView, progressBar, prefUtils);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if(positionOffsetPixels == 0 && pagerPosition != position) {
+                    pagerPosition = position;
+                    if(pagerPosition > 1) {
+                        PostListByCategoryFromApi.getInstance().removeAllDataFromList(prefUtils);
+                        prefUtils.saveSelectedCategoryId(categoryList.get(pagerPosition - 2).getIdCategory());
+
+                        showOrHideItem(View.VISIBLE);
+                        MakePostsByCategoryGridRecyclerViewForTapeActivity
+                                .makePostsByCategoryGridRecyclerViewForTapeActivity(TapeActivity.this, recyclerView, progressBar, prefUtils);
+                    } else {
+                        prefUtils.saveSelectedCategoryId("");
+                        showOrHideItem(View.GONE);
+                    }
                 }
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {}
+            public void onPageSelected(int position) {}
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {}
+            public void onPageScrollStateChanged(int state) {}
         });
+    }
+
+    private void showOrHideItem(int visibility) {
+        recyclerView.setVisibility(visibility);
+        progressBar.setVisibility(visibility);
     }
 
     @Override
@@ -182,7 +115,7 @@ public class TapeActivity extends BaseActivity implements View.OnClickListener{
 
         this.doubleBackToExitPressedOnce = true;
         showToast(getString(R.string.press_twice_to_exit));
-        new Handler().postDelayed(() -> doubleBackToExitPressedOnce=false, 2000);
+        new Handler().postDelayed(() -> doubleBackToExitPressedOnce = false, 2000);
     }
 
     private void removeAllDataFromPostList() {
@@ -216,4 +149,3 @@ public class TapeActivity extends BaseActivity implements View.OnClickListener{
         }
     }
 }
- */
