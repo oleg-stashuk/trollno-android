89ada88fce5135ae5b9657f054aaa6d65a3ad594
Nina <n.kruchinkina22022@gmail.com>
2021-01-15 12:12:23 +0200

    In DiscussPostsAdapter and PostListAdapter add checking for user is guest or not

--- app/src/main/java/com/apps/trollino/adapters/PostListAdapter.java
+++ app/src/main/java/com/apps/trollino/adapters/PostListAdapter.java
@@ -49,7 +49,7 @@ public class PostListAdapter extends BaseRecyclerAdapter<PostsModel.PostDetails>
                 TextView textDiscussImageView = itemView.findViewById(R.id.discuss_text_post_two_columns);
                 TextView titleVideoTextView = itemView.findViewById(R.id.title_post_two_columns);
 
-                if(item.getRead() == 0 && prefUtils.getIsUserAuthorization()) {
+                if(!prefUtils.getIsUserAuthorization() || (item.getRead() == 0 && prefUtils.getIsUserAuthorization())) {
                     linearLayout.setBackgroundColor(ContextCompat.getColor(view.getContext(), R.color.white));
                     titleVideoTextView.setTextColor(ContextCompat.getColor(view.getContext(), R.color.colorText));
                     frameLayout.setVisibility(View.GONE);
