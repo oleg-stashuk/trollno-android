78611e612a06d86812f520f0a84af20dd484241a
Nina <n.kruchinkina22022@gmail.com>
2021-01-21 13:50:53 +0200

    Add include_tape_two_column_shimmer.xml.In ShimmerHide add methods show recyclerView.

--- app/src/main/java/com/apps/trollino/utils/networking_helper/ShimmerHide.java
+++ app/src/main/java/com/apps/trollino/utils/networking_helper/ShimmerHide.java
@@ -4,12 +4,22 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.facebook.shimmer.ShimmerFrameLayout;
 
 public class ShimmerHide {
 
     public static void shimmerHide(LinearLayout layout, ShimmerFrameLayout shimmerLayout) {
         layout.setVisibility(View.VISIBLE);
+        Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            shimmerLayout.setVisibility(View.GONE);
+        }, 1500);
+    }
+
+    public static void shimmerHide(RecyclerView recycler, ShimmerFrameLayout shimmerLayout) {
+        recycler.setVisibility(View.VISIBLE);
         Handler handler = new Handler();
         handler.postDelayed(() -> {
             shimmerLayout.setVisibility(View.GONE);
