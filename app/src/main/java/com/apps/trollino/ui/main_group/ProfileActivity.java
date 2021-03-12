70fe4ae67b19529865b71fa74dd57ad94b0c7f2f
Nina <n.kruchinkina22022@gmail.com>
2021-03-12 14:00:31 +0200

    In build.gradle add dataBinding. In ProfileActivity add dataBinding

--- app/src/main/java/com/apps/trollino/ui/main_group/ProfileActivity.java
+++ app/src/main/java/com/apps/trollino/ui/main_group/ProfileActivity.java
@@ -10,8 +10,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.apps.trollino.R;
+import com.apps.trollino.databinding.ActivityProfileBinding;
 import com.apps.trollino.service.MyFirebaseMessagingService;
 import com.apps.trollino.ui.authorisation.LoginActivity;
 import com.apps.trollino.ui.authorisation.RegistrationActivity;
@@ -27,6 +29,8 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 import static com.apps.trollino.utils.SnackBarMessageCustom.showSnackBarOnTheTopByBottomNavigation;
 
 public class ProfileActivity extends BaseActivity implements View.OnClickListener{
+    private ActivityProfileBinding binding;
+
     private LinearLayout userIncludeLinearLayout;
     private ShimmerFrameLayout userIncludeShimmer;
     private LinearLayout guestIncludeLinearLayout;
@@ -34,7 +38,6 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
     private ImageView userImageView;
     private TextView emailTextView;
     private TextView nameTextView;
-    private Button exitButton;
     private ImageView indicatorImageView;
 
     private boolean doubleBackToExitPressedOnce = false; // для обработки нажатия onBackPressed
@@ -46,6 +49,9 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
 
     @Override
     protected void initView() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
+        prefUtils.saveCurrentActivity(OpenActivityHelper.PROFILE_ACTIVITY);
+
         userIncludeLinearLayout = findViewById(R.id.include_user_profile);
         userIncludeShimmer = findViewById(R.id.include_user_profile_shimmer);
         guestIncludeLinearLayout = findViewById(R.id.include_user_not_authorization_profile);
@@ -53,18 +59,12 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         userImageView = findViewById(R.id.image_account_profile_include);
         nameTextView = findViewById(R.id.name_account_profile_include);
         emailTextView = findViewById(R.id.email_account_profile_include);
-        exitButton = findViewById(R.id.exit_button_profile);
         TextView profileBottomNavigationTextView = findViewById(R.id.profile_button);
         indicatorImageView = findViewById(R.id.indicator_image);
 
-        SwitchMaterial markReadPostSwitch = findViewById(R.id.mark_read_post_switch);
-        SwitchMaterial answerToCommentSwitch = findViewById(R.id.answer_to_comment_switch);
-
         findViewById(R.id.login_button_include_profile_for_guest).setOnClickListener(this);
         findViewById(R.id.registration_button_include_profile_for_guest).setOnClickListener(this);
-        exitButton.setOnClickListener(this);
         userIncludeLinearLayout.setOnClickListener(this);
-        findViewById(R.id.rate_profile).setOnClickListener(this);
         findViewById(R.id.tape_button).setOnClickListener(this);
         findViewById(R.id.activity_button).setOnClickListener(this);
         findViewById(R.id.favorites_button).setOnClickListener(this);
@@ -73,21 +73,9 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         profileBottomNavigationTextView.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_profile_green, 0, 0);
         profileBottomNavigationTextView.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
 
-        prefUtils.saveCurrentActivity(OpenActivityHelper.PROFILE_ACTIVITY);
-
         makeIsUserAuthorizationCorrectData();
-
-        markReadPostSwitch.setChecked(prefUtils.isShowReadPost());
-        markReadPostSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            prefUtils.saveIsShowReadPost(isChecked);
-            //@TODO add PUT to update this data in API
-        });
-
-        answerToCommentSwitch.setChecked(prefUtils.isSendPushAboutAnswerToComment());
-        answerToCommentSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            prefUtils.saveIsSendPushAboutAnswerToComment(isChecked);
-            //@TODO add PUT to update this data in API and add or removeFireBaseToken()
-        });
+        initSwitch();
+        initClickListeners();
     }
 
     private void makeIsUserAuthorizationCorrectData() {
@@ -95,7 +83,7 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
             userIncludeLinearLayout.setVisibility(View.GONE);
             userIncludeShimmer.setVisibility(View.VISIBLE);
             guestIncludeLinearLayout.setVisibility(View.GONE);
-            exitButton.setVisibility(View.VISIBLE);
+            binding.exitButtonProfile.setVisibility(View.VISIBLE);
 
             new Thread(() -> {
                 GetUserProfile.getUserProfile(this, prefUtils, userImageView, nameTextView, emailTextView,
@@ -107,8 +95,42 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
             userIncludeLinearLayout.setVisibility(View.GONE);
             userIncludeShimmer.setVisibility(View.GONE);
             guestIncludeLinearLayout.setVisibility(View.VISIBLE);
-            exitButton.setVisibility(View.GONE);
+            binding.exitButtonProfile.setVisibility(View.GONE);
         }
+    }
+
+    private void initSwitch(){
+        binding.markReadPostSwitch.setChecked(prefUtils.isShowReadPost());
+        binding.markReadPostSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            prefUtils.saveIsShowReadPost(isChecked);
+            //@TODO add PUT to update this data in API
+        });
+
+
+        binding.answerToCommentSwitch.setChecked(prefUtils.isSendPushAboutAnswerToComment());
+        binding.answerToCommentSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            prefUtils.saveIsSendPushAboutAnswerToComment(isChecked);
+            //@TODO add PUT to update this data in API and add or removeFireBaseToken()
+        });
+    }
+
+    private void initClickListeners() {
+        binding.rateProfile.setOnClickListener(v -> {
+            openPlayMarketForRateTheApp();
+        });
+
+        binding.exitButtonProfile.setOnClickListener(v -> {
+            LoginManager.getInstance().logOut();
+            new Thread(
+                    () -> PostLogout.postLogout(this, prefUtils, bottomNavigation)
+            ).start();
+            removeFireBaseToken();
+        });
+
+        binding.includeUserProfile.setOnClickListener(v -> {
+            startActivity(new Intent(this, EditUserProfileActivity.class));
+            finish();
+        });
     }
 
     private void openPlayMarketForRateTheApp() {
@@ -142,27 +164,17 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.include_user_profile:
-                startActivity(new Intent(this, EditUserProfileActivity.class));
-                finish();
-                break;
+//            case R.id.include_user_profile:
+//                startActivity(new Intent(this, EditUserProfileActivity.class));
+//                finish();
+//                break;
             case R.id.login_button_include_profile_for_guest:
                 startActivity(new Intent(this, LoginActivity.class));
                 finish();
                 break;
-            case R.id.exit_button_profile:
-                LoginManager.getInstance().logOut();
-                new Thread(
-                        () -> PostLogout.postLogout(this, prefUtils, bottomNavigation)
-                ).start();
-                removeFireBaseToken();
-                break;
             case R.id.registration_button_include_profile_for_guest:
                 startActivity(new Intent(this, RegistrationActivity.class));
                 finish();
-                break;
-            case R.id.rate_profile:
-                openPlayMarketForRateTheApp();
                 break;
             case R.id.tape_button: // "Перейти на экран Лента"
                 startActivity(new Intent(this, TapeActivity.class));
