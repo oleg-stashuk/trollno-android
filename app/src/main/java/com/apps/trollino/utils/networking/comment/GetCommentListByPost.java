04585ffc29413b34e99d6e099ac319e6fe3c5000
Nina <n.kruchinkina22022@gmail.com>
2021-03-16 10:15:24 +0200

    Fix CommentToPostActivity, GetCommentListByPost, MakeRecyclerViewForComment and activity_comment_to_post.xml

--- app/src/main/java/com/apps/trollino/utils/networking/comment/GetCommentListByPost.java
+++ app/src/main/java/com/apps/trollino/utils/networking/comment/GetCommentListByPost.java
@@ -3,12 +3,9 @@ package com.apps.trollino.utils.networking.comment;
 import android.content.Context;
 import android.util.Log;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.apps.trollino.R;
 import com.apps.trollino.adapters.CommentToPostParentAdapter;
@@ -38,7 +35,7 @@ public class GetCommentListByPost {
     private static int totalPage;
     private static RecyclerView recyclerView;
     private static boolean isGetNewListThis;
-
+    private static Context cont;
 
     public static void getCommentListByPost(Context context, PrefUtils prefUtils,
                                             String postId, String sortBy,
@@ -46,11 +43,10 @@ public class GetCommentListByPost {
                                             CommentToPostParentAdapter adapter, TextView noCommentTextView,
                                             TextView countTextView, boolean isGetNewList) {
 
-        Log.d("OkHttp", "!!!!!!!!!!!!!!!!!!! shimmer - " + (shimmer != null) + " refreshLayout - " + (refreshLayout != null));
-
          String sortOrder = Const.SORT_ORDER_BY_DESC;
 
         recyclerView = recycler;
+        cont = context;
         isGetNewListThis = isGetNewList;
         page = isGetNewList ? 0 : prefUtils.getCurrentPage();
         if(isGetNewList) {
@@ -70,15 +66,13 @@ public class GetCommentListByPost {
 
                     List<CommentModel.Comments> commentList = commentModel.getCommentsList();
                     showCorrectVariant(commentList, adapter, shimmer, noCommentTextView, countTextView);
-                    if(refreshLayout != null) {
-                        refreshLayout.setRefreshing(false);
-                    }
                 } else {
                     String errorMessage = ErrorMessageFromApi.errorMessageFromApi(response.errorBody());
                     SnackBarMessageCustom.showSnackBar(recycler, errorMessage);
                 }
-
-//                progressBar.setVisibility(View.GONE);
+                if(refreshLayout != null) {
+                    refreshLayout.setRefreshing(false);
+                }
             }
 
             @Override
@@ -97,7 +91,6 @@ public class GetCommentListByPost {
                 } else {
                     SnackBarMessageCustom.showSnackBar(recycler, t.getLocalizedMessage());
                 }
-//                progressBar.setVisibility(View.GONE);
                 Log.d(TAG_LOG, "t.getLocalizedMessage() " + t.getLocalizedMessage());
             }
 
@@ -146,119 +139,9 @@ public class GetCommentListByPost {
         int newListSize = CommentListFromApi.getInstance().getCommentList().size();
 
         if(newListSize == currentListSize && page == totalPage && ! isGetNewListThis) {
-            SnackBarMessageCustom.showSnackBar(recyclerView, "Показаны все комментарии к этому посту");
+            SnackBarMessageCustom.showSnackBar(recyclerView, cont.getString(R.string.msg_all_comments_showed));
         }
 
         adapter.notifyDataSetChanged();
     }
 }
-
-/*
-public class GetCommentListByPost {
-    private static int page;
-    private static int totalPage;
-    private static RecyclerView recyclerView;
-    private static boolean isGetNewListThis;
-
-    public static void getCommentListByPost(Context context, PrefUtils prefUtils,
-                                            String postId, String sortBy, String sortOrder,
-                                            RecyclerView recycler, ShimmerFrameLayout shimmer,
-                                            CommentToPostParentAdapter adapter, TextView noCommentTextView,
-                                            TextView countTextView, ProgressBar progressBar, boolean isGetNewList) {
-
-        recyclerView = recycler;
-        isGetNewListThis = isGetNewList;
-        page = isGetNewList ? 0 : prefUtils.getCurrentPage();
-        if(isGetNewList) {
-            CommentListFromApi.getInstance().removeAllDataFromList(prefUtils);
-        }
-        String cookie = prefUtils.getCookie();
-
-        ApiService.getInstance(context).getCommentToPost(cookie, postId, sortBy, sortOrder, new Callback<CommentModel>() {
-            @Override
-            public void onResponse(Call<CommentModel> call, Response<CommentModel> response) {
-                if (response.isSuccessful()) {
-                    CommentModel commentModel = response.body();
-
-                    PagerModel pagerModel = commentModel.getPagerModel();
-                    totalPage = pagerModel.getTotalItems() - 1;
-                    saveCurrentPage(prefUtils);
-
-                    List<CommentModel.Comments> commentList = commentModel.getCommentsList();
-                    showCorrectVariant(commentList, adapter, shimmer, noCommentTextView, countTextView);
-                } else {
-                    String errorMessage = ErrorMessageFromApi.errorMessageFromApi(response.errorBody());
-                    SnackBarMessageCustom.showSnackBar(recycler, errorMessage);
-                }
-
-                progressBar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onFailure(Call<CommentModel> call, Throwable t) {
-                t.getStackTrace();
-                boolean isHaveNotInternet = t.getLocalizedMessage().contains(context.getString(R.string.internet_error_from_api));
-                String noInternetMessage = context.getResources().getString(R.string.internet_error_message);
-                if (isHaveNotInternet) {
-                    Snackbar
-                            .make(recycler, noInternetMessage, Snackbar.LENGTH_INDEFINITE)
-                            .setMaxInlineActionWidth(3)
-                            .setAction(R.string.refresh_button, v -> {
-                                call.clone().enqueue(this);
-                            })
-                            .show();
-                } else {
-                    SnackBarMessageCustom.showSnackBar(recycler, t.getLocalizedMessage());
-                }
-                progressBar.setVisibility(View.GONE);
-                Log.d(TAG_LOG, "t.getLocalizedMessage() " + t.getLocalizedMessage());
-            }
-
-        });
-
-    }
-
-    // Если для Поста нет комментариев, то выводится на экран сообщение что комментариев нет
-    private static void showCorrectVariant(List<CommentModel.Comments> commentList,
-                                           CommentToPostParentAdapter adapter, ShimmerFrameLayout shimmer,
-                                           TextView noCommentTextView, TextView countTextView) {
-        int commentCount = commentList.size();
-
-        if(commentCount > 0) {
-            noCommentTextView.setVisibility(View.GONE);
-
-            for(CommentModel.Comments comment : commentList) {
-                commentCount += Integer.parseInt(comment.getCommentAnswersCount());
-            }
-            updatePostListAndNotifyRecyclerAdapter(commentList, adapter);
-            ShimmerHide.shimmerHide(recyclerView, shimmer);
-        } else {
-            recyclerView.setVisibility(View.GONE);
-            ShimmerHide.shimmerHide(noCommentTextView, shimmer);
-        }
-
-        countTextView.setText(String.valueOf(commentCount));
-    }
-
-    private static void saveCurrentPage(PrefUtils prefUtils) {
-        if(page < totalPage) {
-            prefUtils.saveCurrentPage(page + 1);
-        } else {
-            prefUtils.saveCurrentPage(totalPage);
-        }
-    }
-
-    private static void updatePostListAndNotifyRecyclerAdapter(List<CommentModel.Comments> commentList, CommentToPostParentAdapter adapter) {
-        int currentListSize = CommentListFromApi.getInstance().getCommentList().size();
-        CommentListFromApi.getInstance().saveCommentInList(commentList);
-        int newListSize = CommentListFromApi.getInstance().getCommentList().size();
-
-        if(newListSize == currentListSize && page == totalPage && ! isGetNewListThis) {
-            SnackBarMessageCustom.showSnackBar(recyclerView, "Показаны все комментарии к этому посту");
-        }
-
-        adapter.notifyDataSetChanged();
-    }
-}
-
- */
--- app/src/main/java/com/apps/trollino/utils/recycler/MakeRecyclerViewForComment.java
+++ app/src/main/java/com/apps/trollino/utils/recycler/MakeRecyclerViewForComment.java
@@ -2,20 +2,15 @@ package com.apps.trollino.utils.recycler;
 
 import android.content.Context;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.apps.trollino.adapters.CommentToPostParentAdapter;
 import com.apps.trollino.ui.base.BaseActivity;
-import com.apps.trollino.utils.data.Const;
-import com.apps.trollino.utils.RecyclerScrollListener;
 import com.apps.trollino.utils.data.CommentListFromApi;
 import com.apps.trollino.utils.data.PrefUtils;
 import com.apps.trollino.utils.networking.comment.GetCommentListByPost;
@@ -70,7 +65,6 @@ public class MakeRecyclerViewForComment {
                                        CommentToPostParentAdapter adapter, String postId,
                                        TextView noCommentTextView, TextView countTextView, String sortBy,
                                        boolean isGetNewList) {
-//        progressBar.setVisibility(isGetNewList ? View.GONE : View.VISIBLE);
         if(shimmer != null) {
             shimmer.setVisibility(isGetNewList ? View.VISIBLE : View.GONE);
         }
@@ -80,74 +74,3 @@ public class MakeRecyclerViewForComment {
     }
 
 }
-
-/*
-public class MakeRecyclerViewForComment extends RecyclerView.OnScrollListener{
-    private static Context cont;
-    private static PrefUtils prefUt;
-    private static String sortOrder = Const.SORT_ORDER_BY_DESC;
-
-    public static void makeRecyclerViewForComment(Context context, PrefUtils prefUtils,
-                                                  RecyclerView recyclerView, ShimmerFrameLayout shimmer,
-                                                  ProgressBar progressBar, String postId, EditText commentEditText,
-                                                  TextView noCommentTextView, TextView countTextView, String sortBy) {
-
-        cont = context;
-        prefUt = prefUtils;
-
-        CommentToPostParentAdapter adapter = new CommentToPostParentAdapter((BaseActivity) context,
-                prefUtils, CommentListFromApi.getInstance().getCommentList(), commentEditText);
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setHasFixedSize(true);
-
-        progressBar.setVisibility(View.VISIBLE);
-        if(CommentListFromApi.getInstance().getCommentList().isEmpty()) {
-            infiniteScroll(progressBar, recyclerView, shimmer, adapter, postId, noCommentTextView,
-                    countTextView, sortBy, true);
-        }
-
-        recyclerView.addOnScrollListener(new RecyclerScrollListener() {
-            @Override
-            public void onScrolledToEnd() {
-                infiniteScroll(progressBar, recyclerView, shimmer, adapter, postId, noCommentTextView,
-                        countTextView, sortBy, false);
-            }
-
-            @Override
-            public void onScrolledToTop() {
-//                infiniteScroll(progressBar, recyclerView, shimmer, adapter, postId, noCommentTextView,
-//                        countTextView, sortBy, true);
-            }
-        });
-
-    }
-
-    // Загрузить/обновить данные с API
-    private static void updateDataFromApi(ProgressBar progressBar, RecyclerView recyclerView, ShimmerFrameLayout shimmer,
-                                          CommentToPostParentAdapter adapter, String postId,
-                                          TextView noCommentTextView, TextView countTextView, String sortBy,
-                                          boolean isGetNewList) {
-        new Thread(() -> {
-            GetCommentListByPost.getCommentListByPost(cont, prefUt, postId, sortBy, sortOrder,
-                    recyclerView, shimmer, adapter,
-                    noCommentTextView, countTextView, progressBar, isGetNewList);
-        }).start();
-    }
-
-    // Загрузить/обновить данные с API при скролах ресайклера вверх или вниз, если достигнут конец списка
-    private static void infiniteScroll(ProgressBar progressBar, RecyclerView recyclerView, ShimmerFrameLayout shimmer,
-                                       CommentToPostParentAdapter adapter, String postId,
-                                       TextView noCommentTextView, TextView countTextView, String sortBy,
-                                       boolean isGetNewList) {
-        progressBar.setVisibility(isGetNewList ? View.GONE : View.VISIBLE);
-        shimmer.setVisibility(isGetNewList ? View.VISIBLE : View.GONE);
-        Handler handler = new Handler();
-        handler.postDelayed(() -> updateDataFromApi(progressBar, recyclerView, shimmer, adapter, postId,
-                noCommentTextView, countTextView, sortBy, isGetNewList), 1000);
-    }
-
-}
- */
