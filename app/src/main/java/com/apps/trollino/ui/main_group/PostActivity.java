841b01ce9925ca303135c14d408362ee492b29a6
Nina <n.kruchinkina22022@gmail.com>
2021-03-23 09:52:50 +0200

    Remove to add binding to PostActivity and GetItemPost.

--- app/src/main/java/com/apps/trollino/ui/main_group/PostActivity.java
+++ app/src/main/java/com/apps/trollino/ui/main_group/PostActivity.java
@@ -4,152 +4,26 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
-import androidx.databinding.DataBindingUtil;
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.apps.trollino.R;
-import com.apps.trollino.databinding.IncludeContentScrollingPostBinding;
 import com.apps.trollino.ui.base.BaseActivity;
 import com.apps.trollino.utils.OpenActivityHelper;
 import com.apps.trollino.utils.ShowAdvertising;
+import com.apps.trollino.utils.SnackBarMessageCustom;
 import com.apps.trollino.utils.dialogs.GuestDialog;
 import com.apps.trollino.utils.networking.single_post.GetItemPost;
 import com.apps.trollino.utils.networking.single_post.PostBookmark;
 import com.apps.trollino.utils.networking.single_post.PostUnbookmark;
+import com.facebook.shimmer.ShimmerFrameLayout;
 
-public class PostActivity extends BaseActivity{
-    private IncludeContentScrollingPostBinding binding;
-
-    private Menu menu;
-    private String currentPostId;
-
-    @Override
-    protected int getLayoutID() {
-        return R.layout.activity_post;
-    }
-
-    @Override
-    protected void initView() {
-        binding = DataBindingUtil.setContentView(this, getLayoutID());
-
-        initToolbar();
-        makeClickListener();
-        RelativeLayout advRelativeLayout = findViewById(R.id.ad_mob_in_post);
-
-        binding.includeShimmerPost.setVisibility(View.VISIBLE);
-        ShowAdvertising.showAdvertising(advRelativeLayout, prefUtils, this);
-
-        prefUtils.saveCurrentActivity(OpenActivityHelper.POST_ACTIVITY);
-        currentPostId = prefUtils.getCurrentPostId();
-
-        binding.categoryPostActivity.setFocusable(true);
-    }
-
-    // Иницировать Toolbar
-    private void initToolbar() {
-        final Toolbar toolbar = findViewById(R.id.toolbar_post);
-        setSupportActionBar(toolbar);
-
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // отображать кнопку BackPress
-            getSupportActionBar().setHomeButtonEnabled(true); // вернуться на предыдущую активность
-            getSupportActionBar().setDisplayShowTitleEnabled(false); // отображать Заголовок
-        }
-    }
-
-    // Добавить Menu
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.post_menu, menu);
-        this.menu = menu;
-        getPostFromAPi(currentPostId);
-        return true;
-    }
-
-    // Get post data by Id from API
-    private void getPostFromAPi(String postId) {
-        binding.nestedScrollInPost.smoothScrollTo(0, binding.nestedScrollInPost.getTop());
-        binding.postLayout.setVisibility(View.GONE);
-        binding.includeShimmerPost.setVisibility(View.VISIBLE);
-        new Thread(() -> {
-            GetItemPost.getItemPost(this, prefUtils, menu, binding, postId);
-        }).start();
-    }
-
-    // Open activity with category
-    private void commentToPostActivity() {
-        prefUtils.saveCommentIdForActivity("");
-        prefUtils.saveCurrentPostId(currentPostId);
-        prefUtils.saveValuePostFromCategoryList(prefUtils.IsPostFromCategoryList());
-        prefUtils.saveCurrentActivity("");
-        startActivity(new Intent( this, CommentToPostActivity.class));
-        finish();
-    }
-
-    // Press "favorite" button
-    private void pressFavoriteButton() {
-        if (!prefUtils.getCookie().isEmpty()) {
-            if (prefUtils.getIsFavorite()) {
-                new Thread(() -> PostUnbookmark.removePostFromFavorite(this, prefUtils, currentPostId, menu, findViewById(R.id.activity_post))).start();
-            } else {
-                new Thread(() -> PostBookmark.addPostToFavorite(this, prefUtils, currentPostId, menu, findViewById(R.id.activity_post))).start();
-            }
-        } else {
-            GuestDialog dialog = new GuestDialog();
-            dialog.showDialog(this);
-        }
-    }
-
-    // Обрабтка нажантия на выпадающий список из Menu
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                break;
-            case R.id.comment_button:
-                commentToPostActivity();
-                break;
-            case R.id.favorite_button:
-                pressFavoriteButton();
-                break;
-            }
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        prefUtils.saveCurrentActivity("");
-        startActivity(new Intent(this, TapeActivity.class));
-        finish();
-    }
-
-    private void makeClickListener() {
-        binding.addCommentButtonPostActivity.setOnClickListener(v -> {
-            commentToPostActivity();
-        });
-
-        binding.prevPost.setOnClickListener(v -> {
-            String prevPostId = prefUtils.getPrevPostId();
-            if(!prevPostId.isEmpty() && prevPostId.length() > 0 && !prevPostId.equals("0")) {
-                getPostFromAPi(prevPostId);
-                currentPostId = prevPostId;
-            }
-        });
-
-        binding.nextPost.setOnClickListener(v -> {
-            String nextPostId = prefUtils.getNextPostId();
-            if(!nextPostId.isEmpty() && nextPostId.length() > 0 && !nextPostId.equals("0")) {
-                getPostFromAPi(nextPostId);
-                currentPostId = nextPostId;
-            }
-        });
-    }
-}
-
-/*
 public class PostActivity extends BaseActivity implements View.OnClickListener{
     private NestedScrollView nestedScrollView;
     private LinearLayout postLayout;
@@ -174,7 +48,7 @@ public class PostActivity extends BaseActivity implements View.OnClickListener{
     protected void initView() {
         initToolbar();
 
-        nestedScrollView = findViewById(R.id.include_post_screen);
+        nestedScrollView = findViewById(R.id.nested_scroll_in_post);
         postLayout = findViewById(R.id.post_layout);
         shimmerLayout = findViewById(R.id.include_shimmer_post);
         findViewById(R.id.prev_post).setOnClickListener(this);
@@ -291,7 +165,7 @@ public class PostActivity extends BaseActivity implements View.OnClickListener{
                     getPostFromAPi(prevPostId);
                     currentPostId = prevPostId;
                 } else {
-
+                    SnackBarMessageCustom.showSnackBar(postLayout, getString(R.string.first_post_in_category));
                 }
                 break;
             case R.id.next_post:
@@ -299,9 +173,143 @@ public class PostActivity extends BaseActivity implements View.OnClickListener{
                 if(!nextPostId.isEmpty() && nextPostId.length() > 0 && !nextPostId.equals("0")) {
                     getPostFromAPi(nextPostId);
                     currentPostId = nextPostId;
+                } else {
+                    SnackBarMessageCustom.showSnackBar(postLayout, getString(R.string.last_post_in_category));
                 }
                 break;
         }
     }
 }
- */
+
+/*
+public class PostActivity extends BaseActivity{
+    private IncludeContentScrollingPostBinding binding;
+
+    private Menu menu;
+    private String currentPostId;
+
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_post;
+    }
+
+    @Override
+    protected void initView() {
+        binding = DataBindingUtil.setContentView(this, getLayoutID());
+        shimmer = findViewById(R.id.include_shimmer_post);
+
+        initToolbar();
+        makeClickListener();
+        RelativeLayout advRelativeLayout = findViewById(R.id.ad_mob_in_post);
+
+        binding.includeShimmerPost.setVisibility(View.VISIBLE);
+        ShowAdvertising.showAdvertising(advRelativeLayout, prefUtils, this);
+
+        prefUtils.saveCurrentActivity(OpenActivityHelper.POST_ACTIVITY);
+        currentPostId = prefUtils.getCurrentPostId();
+
+        binding.categoryPostActivity.setFocusable(true);
+    }
+
+    // Иницировать Toolbar
+    private void initToolbar() {
+        final Toolbar toolbar = findViewById(R.id.toolbar_post);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true); // отображать кнопку BackPress
+            getSupportActionBar().setHomeButtonEnabled(true); // вернуться на предыдущую активность
+            getSupportActionBar().setDisplayShowTitleEnabled(false); // отображать Заголовок
+        }
+    }
+
+    // Добавить Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.post_menu, menu);
+        this.menu = menu;
+        getPostFromAPi(currentPostId);
+        return true;
+    }
+
+    // Get post data by Id from API
+    private void getPostFromAPi(String postId) {
+        binding.nestedScrollInPost.smoothScrollTo(0, binding.nestedScrollInPost.getTop());
+        binding.postLayout.setVisibility(View.GONE);
+        binding.includeShimmerPost.setVisibility(View.VISIBLE);
+        new Thread(() -> {
+            GetItemPost.getItemPost(this, prefUtils, menu, binding, postId);
+        }).start();
+    }
+
+    // Open activity with category
+    private void commentToPostActivity() {
+        prefUtils.saveCommentIdForActivity("");
+        prefUtils.saveCurrentPostId(currentPostId);
+        prefUtils.saveValuePostFromCategoryList(prefUtils.IsPostFromCategoryList());
+        prefUtils.saveCurrentActivity("");
+        startActivity(new Intent( this, CommentToPostActivity.class));
+        finish();
+    }
+
+    // Press "favorite" button
+    private void pressFavoriteButton() {
+        if (!prefUtils.getCookie().isEmpty()) {
+            if (prefUtils.getIsFavorite()) {
+                new Thread(() -> PostUnbookmark.removePostFromFavorite(this, prefUtils, currentPostId, menu, findViewById(R.id.activity_post))).start();
+            } else {
+                new Thread(() -> PostBookmark.addPostToFavorite(this, prefUtils, currentPostId, menu, findViewById(R.id.activity_post))).start();
+            }
+        } else {
+            GuestDialog dialog = new GuestDialog();
+            dialog.showDialog(this);
+        }
+    }
+
+    // Обрабтка нажантия на выпадающий список из Menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            case R.id.comment_button:
+                commentToPostActivity();
+                break;
+            case R.id.favorite_button:
+                pressFavoriteButton();
+                break;
+            }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        prefUtils.saveCurrentActivity("");
+        startActivity(new Intent(this, TapeActivity.class));
+        finish();
+    }
+
+    private void makeClickListener() {
+        binding.addCommentButtonPostActivity.setOnClickListener(v -> {
+            commentToPostActivity();
+        });
+
+        binding.prevPost.setOnClickListener(v -> {
+            String prevPostId = prefUtils.getPrevPostId();
+            if(!prevPostId.isEmpty() && prevPostId.length() > 0 && !prevPostId.equals("0")) {
+                getPostFromAPi(prevPostId);
+                currentPostId = prevPostId;
+            }
+        });
+
+        binding.nextPost.setOnClickListener(v -> {
+            String nextPostId = prefUtils.getNextPostId();
+            if(!nextPostId.isEmpty() && nextPostId.length() > 0 && !nextPostId.equals("0")) {
+                getPostFromAPi(nextPostId);
+                currentPostId = nextPostId;
+            }
+        });
+    }
+}
+*/
--- app/src/main/java/com/apps/trollino/utils/networking/single_post/GetItemPost.java
+++ app/src/main/java/com/apps/trollino/utils/networking/single_post/GetItemPost.java
@@ -1,10 +1,12 @@
 package com.apps.trollino.utils.networking.single_post;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,12 +17,12 @@ import com.apps.trollino.adapters.OnePostElementAdapter;
 import com.apps.trollino.data.model.CategoryModel;
 import com.apps.trollino.data.model.single_post.ItemPostModel;
 import com.apps.trollino.data.networking.ApiService;
-import com.apps.trollino.databinding.IncludeContentScrollingPostBinding;
 import com.apps.trollino.ui.base.BaseActivity;
 import com.apps.trollino.utils.SnackBarMessageCustom;
 import com.apps.trollino.utils.data.PrefUtils;
 import com.apps.trollino.utils.networking_helper.ErrorMessageFromApi;
 import com.apps.trollino.utils.networking_helper.ShimmerHide;
+import com.facebook.shimmer.ShimmerFrameLayout;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
@@ -30,158 +32,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 import static com.apps.trollino.utils.data.Const.TAG_LOG;
-
-public class GetItemPost {
-    @SuppressLint("StaticFieldLeak")
-    private static Context cont;
-    private static ItemPostModel model;
-    private static PrefUtils prefUt;
-
-    @SuppressLint("StaticFieldLeak")
-    private static IncludeContentScrollingPostBinding bindingInRequest;
-
-    public static void getItemPost(Context context, PrefUtils prefUtils, Menu menu,
-                                   IncludeContentScrollingPostBinding binding, String postId) {
-        bindingInRequest = binding;
-        cont = context;
-        prefUt = prefUtils;
-        String cookie = prefUtils.getCookie();
-
-        ApiService.getInstance(context).getItemPost(cookie, postId, new Callback<ItemPostModel>() {
-            @Override
-            public void onResponse(Call<ItemPostModel> call, Response<ItemPostModel> response) {
-                if(response.isSuccessful()) {
-                    model = response.body();
-                    if (prefUtils.getIsUserAuthorization() && ! model.isReadByUser()) {
-                        new Thread(() -> PostMarkPostAsRead.postMarkPostAsRead(context, prefUtils, postId)).start();
-                    }
-
-                    setPostCategory();
-                    bindingInRequest.titlePostActivity.setText(model.getTitle().get(0).getTitle());
-                    setPostHeadText();
-                    setCommentCount();
-                    saveNextAndPrevPostId();
-                    makePartOfPostRecyclerView();
-
-                    changeImageFavoriteButton(menu);
-                    prefUtils.saveIsFavorite(model.isFavorite());
-
-                    ShimmerHide.shimmerHide(binding.postLayout, binding.includeShimmerPost);
-                } else {
-                    String errorMessage = ErrorMessageFromApi.errorMessageFromApi(response.errorBody());
-                    SnackBarMessageCustom.showSnackBar(binding.nestedScrollInPost, errorMessage);
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ItemPostModel> call, Throwable t) {
-                t.getStackTrace();
-                boolean isHaveNotInternet = t.getLocalizedMessage().contains(context.getString(R.string.internet_error_from_api));
-                String noInternetMessage = context.getResources().getString(R.string.internet_error_message);
-                if (isHaveNotInternet) {
-                    Snackbar
-                            .make(binding.nestedScrollInPost, noInternetMessage, Snackbar.LENGTH_INDEFINITE)
-                            .setMaxInlineActionWidth(3)
-                            .setAction(R.string.refresh_button, v -> {
-                                call.clone().enqueue(this);
-                            })
-                            .show();
-                } else {
-                    SnackBarMessageCustom.showSnackBar(binding.nestedScrollInPost, t.getLocalizedMessage());
-                }
-                Log.d(TAG_LOG, "t.getLocalizedMessage() " + t.getLocalizedMessage());
-            }
-        });
-
-    }
-
-    private static void setPostCategory() {
-        String categoryId = "";
-        for(ItemPostModel.CategoryPost id : model.getCategory()) {
-            categoryId = String.valueOf(id.getIdCategory());
-        }
-
-        for(CategoryModel category : prefUt.getCategoryList()) {
-            if (categoryId.equals(category.getIdCategory())){
-                bindingInRequest.categoryPostActivity.setText(category.getNameCategory());
-            }
-        }
-    }
-
-    private static void setPostHeadText() {
-        String text = model.getBody().get(0).getTextPostBody();
-        if (text.isEmpty()) {
-            bindingInRequest.bodyPostActivity.setVisibility(View.GONE);
-        } else {
-            bindingInRequest.bodyPostActivity.setVisibility(View.VISIBLE);
-            bindingInRequest.bodyPostActivity.setText(text);
-        }
-    }
-
-    private static void setCommentCount() {
-        int commentCount = model.getComment().get(0).getCommentCont();
-        if(commentCount > 0) {
-            bindingInRequest.commentCountPostActivity.setVisibility(View.VISIBLE);
-            bindingInRequest.commentCountPostActivity.setText(String.valueOf(commentCount));
-            bindingInRequest.addCommentButtonPostActivity.setText(R.string.read_the_comment);
-        } else {
-            bindingInRequest.commentCountPostActivity.setVisibility(View.GONE);
-            bindingInRequest.addCommentButtonPostActivity.setText(R.string.write_the_comment);
-        }
-    }
-
-    private static void saveNextAndPrevPostId() {
-        int prevPost;
-        int nextPost;
-        if(prefUt.IsPostFromCategoryList()) {
-            prevPost = model.getNextPost().getCategory().get(0).getIdPost();
-            nextPost = model.getPrevPost().getCategory().get(0).getIdPost();
-        } else {
-            prevPost = model.getNextPost().getPubl().get(0).getIdPost();
-            nextPost = model.getPrevPost().getPubl().get(0).getIdPost();
-        }
-        prefUt.savePrevPostId(String.valueOf(prevPost));
-        prefUt.saveNextPostId(String.valueOf(nextPost));
-        if (prevPost == 0) {
-            bindingInRequest.prevPost.setBackgroundColor(R.drawable.button_background_white);
-            bindingInRequest.prevPost.setClickable(false);
-        } else {
-            bindingInRequest.prevPost.setBackgroundColor(R.drawable.button_background);
-            bindingInRequest.prevPost.setClickable(true);
-        }
-        if (nextPost == 0) {
-            bindingInRequest.nextPost.setBackgroundColor(R.drawable.button_background_white);
-            bindingInRequest.nextPost.setClickable(false);
-        } else {
-            bindingInRequest.nextPost.setBackgroundColor(R.drawable.button_background);
-            bindingInRequest.nextPost.setClickable(true);
-        }
-    }
-
-    private static void makePartOfPostRecyclerView() {
-        List<ItemPostModel.MediaBlock> mediaBlock = model.getMediaBlock();
-        RecyclerView recyclerView = bindingInRequest.recyclerPostActivity;
-        recyclerView.setLayoutManager(new LinearLayoutManager(cont));
-        recyclerView.setAdapter(new OnePostElementAdapter((BaseActivity) cont, mediaBlock, prefUt));
-        recyclerView.setFocusable(false);
-    }
-
-    // Смена картинки для кнопки favorite из menu в ToolBar
-    private static void changeImageFavoriteButton(Menu menu) {
-        try {
-            if (model.isFavorite()) {
-                menu.getItem(1).setIcon(ContextCompat.getDrawable(cont, R.drawable.ic_favorite_button));
-            } else {
-                menu.getItem(1).setIcon(ContextCompat.getDrawable(cont, R.drawable.ic_favorite_border_button));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.d(TAG_LOG, e.getLocalizedMessage());
-        }
-    }
-}
-
-/*
 
 public class GetItemPost {
     private static Context cont;
@@ -343,4 +193,154 @@ public class GetItemPost {
     }
 }
 
- */
+
+/*
+public class GetItemPost {
+    private static Context cont;
+    private static ItemPostModel model;
+    private static PrefUtils prefUt;
+
+    private static IncludeContentScrollingPostBinding bindingInRequest;
+
+    public static void getItemPost(Context context, PrefUtils prefUtils, Menu menu,
+                                   IncludeContentScrollingPostBinding binding, String postId) {
+        bindingInRequest = binding;
+        cont = context;
+        prefUt = prefUtils;
+        String cookie = prefUtils.getCookie();
+
+        ApiService.getInstance(context).getItemPost(cookie, postId, new Callback<ItemPostModel>() {
+            @Override
+            public void onResponse(Call<ItemPostModel> call, Response<ItemPostModel> response) {
+                if(response.isSuccessful()) {
+                    model = response.body();
+                    if (prefUtils.getIsUserAuthorization() && ! model.isReadByUser()) {
+                        new Thread(() -> PostMarkPostAsRead.postMarkPostAsRead(context, prefUtils, postId)).start();
+                    }
+
+                    setPostCategory();
+                    bindingInRequest.titlePostActivity.setText(model.getTitle().get(0).getTitle());
+                    setPostHeadText();
+                    setCommentCount();
+                    saveNextAndPrevPostId();
+                    makePartOfPostRecyclerView();
+
+                    changeImageFavoriteButton(menu);
+                    prefUtils.saveIsFavorite(model.isFavorite());
+
+//                    ShimmerHide.shimmerHide(binding.postLayout, binding.includeShimmerPost);
+                } else {
+                    String errorMessage = ErrorMessageFromApi.errorMessageFromApi(response.errorBody());
+                    SnackBarMessageCustom.showSnackBar(binding.nestedScrollInPost, errorMessage);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ItemPostModel> call, Throwable t) {
+                t.getStackTrace();
+                boolean isHaveNotInternet = t.getLocalizedMessage().contains(context.getString(R.string.internet_error_from_api));
+                String noInternetMessage = context.getResources().getString(R.string.internet_error_message);
+                if (isHaveNotInternet) {
+                    Snackbar
+                            .make(binding.nestedScrollInPost, noInternetMessage, Snackbar.LENGTH_INDEFINITE)
+                            .setMaxInlineActionWidth(3)
+                            .setAction(R.string.refresh_button, v -> {
+                                call.clone().enqueue(this);
+                            })
+                            .show();
+                } else {
+                    SnackBarMessageCustom.showSnackBar(binding.nestedScrollInPost, t.getLocalizedMessage());
+                }
+                Log.d(TAG_LOG, "t.getLocalizedMessage() " + t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+    private static void setPostCategory() {
+        String categoryId = "";
+        for(ItemPostModel.CategoryPost id : model.getCategory()) {
+            categoryId = String.valueOf(id.getIdCategory());
+        }
+
+        for(CategoryModel category : prefUt.getCategoryList()) {
+            if (categoryId.equals(category.getIdCategory())){
+                bindingInRequest.categoryPostActivity.setText(category.getNameCategory());
+            }
+        }
+    }
+
+    private static void setPostHeadText() {
+        String text = model.getBody().get(0).getTextPostBody();
+        if (text.isEmpty()) {
+            bindingInRequest.bodyPostActivity.setVisibility(View.GONE);
+        } else {
+            bindingInRequest.bodyPostActivity.setVisibility(View.VISIBLE);
+            bindingInRequest.bodyPostActivity.setText(text);
+        }
+    }
+
+    private static void setCommentCount() {
+        int commentCount = model.getComment().get(0).getCommentCont();
+        if(commentCount > 0) {
+            bindingInRequest.commentCountPostActivity.setVisibility(View.VISIBLE);
+            bindingInRequest.commentCountPostActivity.setText(String.valueOf(commentCount));
+            bindingInRequest.addCommentButtonPostActivity.setText(R.string.read_the_comment);
+        } else {
+            bindingInRequest.commentCountPostActivity.setVisibility(View.GONE);
+            bindingInRequest.addCommentButtonPostActivity.setText(R.string.write_the_comment);
+        }
+    }
+
+    private static void saveNextAndPrevPostId() {
+        int prevPost;
+        int nextPost;
+        if(prefUt.IsPostFromCategoryList()) {
+            prevPost = model.getNextPost().getCategory().get(0).getIdPost();
+            nextPost = model.getPrevPost().getCategory().get(0).getIdPost();
+        } else {
+            prevPost = model.getNextPost().getPubl().get(0).getIdPost();
+            nextPost = model.getPrevPost().getPubl().get(0).getIdPost();
+        }
+        prefUt.savePrevPostId(String.valueOf(prevPost));
+        prefUt.saveNextPostId(String.valueOf(nextPost));
+        if (prevPost == 0) {
+            bindingInRequest.prevPost.setBackgroundColor(R.drawable.button_background_white);
+            bindingInRequest.prevPost.setClickable(false);
+        } else {
+            bindingInRequest.prevPost.setBackgroundColor(R.drawable.button_background);
+            bindingInRequest.prevPost.setClickable(true);
+        }
+        if (nextPost == 0) {
+            bindingInRequest.nextPost.setBackgroundColor(R.drawable.button_background_white);
+            bindingInRequest.nextPost.setClickable(false);
+        } else {
+            bindingInRequest.nextPost.setBackgroundColor(R.drawable.button_background);
+            bindingInRequest.nextPost.setClickable(true);
+        }
+    }
+
+    private static void makePartOfPostRecyclerView() {
+        List<ItemPostModel.MediaBlock> mediaBlock = model.getMediaBlock();
+        RecyclerView recyclerView = bindingInRequest.recyclerPostActivity;
+        recyclerView.setLayoutManager(new LinearLayoutManager(cont));
+        recyclerView.setAdapter(new OnePostElementAdapter((BaseActivity) cont, mediaBlock, prefUt));
+        recyclerView.setFocusable(false);
+    }
+
+    // Смена картинки для кнопки favorite из menu в ToolBar
+    private static void changeImageFavoriteButton(Menu menu) {
+        try {
+            if (model.isFavorite()) {
+                menu.getItem(1).setIcon(ContextCompat.getDrawable(cont, R.drawable.ic_favorite_button));
+            } else {
+                menu.getItem(1).setIcon(ContextCompat.getDrawable(cont, R.drawable.ic_favorite_border_button));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d(TAG_LOG, e.getLocalizedMessage());
+        }
+    }
+}
+
+*/
