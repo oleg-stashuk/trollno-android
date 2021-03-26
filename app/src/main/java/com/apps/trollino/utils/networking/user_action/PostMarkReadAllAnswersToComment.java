8a4e6b2e7dd27f41a2e9c8a4be87fa0dabcfb448
Nina <n.kruchinkina22022@gmail.com>
2021-03-26 20:11:55 +0200

    Fix in design for CommentToPostActivity and AnswersActivity. Change text in include_no_data_for_user_in_answers.xml

--- app/src/main/java/com/apps/trollino/utils/networking/user_action/PostMarkReadAllAnswersToComment.java
+++ app/src/main/java/com/apps/trollino/utils/networking/user_action/PostMarkReadAllAnswersToComment.java
@@ -15,15 +15,16 @@ import retrofit2.Response;
 import static com.apps.trollino.utils.data.Const.TAG_LOG;
 
 public class PostMarkReadAllAnswersToComment {
-    public static void PostMarkReadAllAnswersToComment(Context context, PrefUtils prefUtils, String commentId) {
+    public static void PostMarkReadAllAnswersToComment(Context context, PrefUtils prefUtils) {
         String cooke = prefUtils.getCookie();
         String token = prefUtils.getToken();
+        String commentId = prefUtils.getCommentIdForActivity();
 
         ApiService.getInstance(context).markReadAllAnswersToComment(cooke, token, commentId, new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
                 if(response.isSuccessful()) {
-                    Log.d(TAG_LOG, "PostMarkReadAllAnswersToComment isSuccessful ");
+                    Log.d(TAG_LOG, "!!!!!!!!!!!! PostMarkReadAllAnswersToComment isSuccessful ");
                 } else if(response.code() == 403) {
                     GuestDialog dialog = new GuestDialog();
                     dialog.showDialog(context);
