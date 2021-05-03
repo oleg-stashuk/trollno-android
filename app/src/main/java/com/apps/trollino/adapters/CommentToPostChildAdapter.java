9905b2d0e70c0e0830920b772bc37514995b74f6
Nina <n.kruchinkina22022@gmail.com>
2021-05-04 00:32:49 +0300

    Commit for production branch

--- app/src/main/java/com/apps/trollino/adapters/CommentToPostChildAdapter.java
+++ app/src/main/java/com/apps/trollino/adapters/CommentToPostChildAdapter.java
@@ -24,6 +24,7 @@ import static com.apps.trollino.utils.data.Const.BASE_URL;
 import static com.apps.trollino.utils.data.Const.COUNT_SYMBOL_TO_HIDE_PAR_OF_COMMENT;
 
 public class CommentToPostChildAdapter extends BaseRecyclerAdapter<CommentModel.Comments> {
+
     private EditText commentEditText;
     private PrefUtils prefUtils;
 
